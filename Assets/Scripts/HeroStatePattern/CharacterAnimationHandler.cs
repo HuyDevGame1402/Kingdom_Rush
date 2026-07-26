0f2bb208f582791b50e03e4110634109e86a85f0
HuyDevGame1402 <huyco14022004@gmail.com>
2026-07-26 21:01:54 +0700

    Refactor Animation Character

---
+++ Assets/Scripts/HeroStatePattern/CharacterAnimationHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterAnimationHandler : BaseUnitAnimationHandler
+{
+    [SerializeField] private string animationID;
+
+    public string AnimationID
+    {
+        get => animationID;
+        set => animationID = value;
+    }
+
+    public override void PlayIdleAnimation(UnitAnimationConfig animData, GameObject target)
+    {
+        var config = animData.idle;
+        CharacterSpriteAnimator.Instance.PlayAnimationByRange(
+            target: target,
+            enemyId: animationID,
+            animPrefix: animData.animPrefix,
+            rangeConfig: config,
+            frameRate: animData.frameRate
+        );
+    }
+
+    public override void PlayRunAnimation(UnitAnimationConfig animData, GameObject target)
+    {
+        var config = animData.run;
+        CharacterSpriteAnimator.Instance.PlayAnimationByRange(
+            target: target,
+            enemyId: animationID,
+            animPrefix: animData.animPrefix,
+            rangeConfig: config,
+            frameRate: animData.frameRate
+        );
+    }
+
+    public override void PlayAttackAnimation(
+        UnitAnimationConfig animData,
+        GameObject target,
+        Action onEventTrigger,
+        Action onComplete)
+    {
+        AnimationFrameRange config = (animData.attacks != null && animData.attacks.Count > 0)
+            ? animData.GetRandomAttack()
+            : animData.attack;
+
+        // Lưu ý: Nếu muốn kích hoạt onEventTrigger đúng frame thì có thể bổ sung callback vào Routine, 
+        // ở đây truyền callback hoàn thành (onComplete) vào animator.
+        CharacterSpriteAnimator.Instance.PlayAnimationByRange(
+            target: target,
+            enemyId: animationID,
+            animPrefix: animData.animPrefix,
+            rangeConfig: config,
+            frameRate: animData.frameRate,
+            onComplete: onComplete
+        );
+    }
+
+    public override void PlayDeathAnimation(UnitAnimationConfig animData, GameObject target, Action onComplete)
+    {
+        var config = animData.death;
+        CharacterSpriteAnimator.Instance.PlayAnimationByRange(
+            target: target,
+            enemyId: animationID,
+            animPrefix: animData.animPrefix,
+            rangeConfig: config,
+            frameRate: animData.frameRate,
+            onComplete: onComplete
+        );
+    }
+}
